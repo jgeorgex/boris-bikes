921c5c1a466b11f0726c2778ed3ce459ec59b021
Valeria Serci <balerty84@gmail.com>
2019-08-21 17:00:11 +0100

    Got the working test for release_bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,15 +1,5 @@
 require 'docking_station'
 
 describe DockingStation do
-  it 'releases bike' do
-  expect (docking_station).
-  (respond_to.)
-end
-
-describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
-  end
-end
-
+  it { is_expected.to respond_to(:release_bike)}
 end
