8252a364611c4eba2f538f29dbb61afa9d0ff094
Julian George <jules104@gmail.com>
2019-08-21 16:40:52 +0100

    Update spec test for release bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,15 @@
 require 'docking_station'
 
 describe DockingStation do
+  it 'releases bike' do
+  expect (docking_station).
+  (respond_to.)
+end
+
+describe 'fizzbuzz' do
+  it 'returns "fizz" when passed 3' do
+    expect(fizzbuzz(3)).to eq 'fizz'
+  end
+end
 
 end
