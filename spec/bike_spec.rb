6a72a890228095e53f9bf446b704af2bef350e27
Thomas Ross <thomasross3610@gmail.com>
2019-08-22 16:53:58 +0100

    bike spec updated with working? method

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,7 +1,7 @@
 require 'bike'
 
 describe Bike do
-   it "expects bike to respond to working method" do
-   expect (Bike).respond_to? :working?
+   # it "expects bike to respond to working method" do
+   it { is_expected.to respond_to (:working?) }
  end
-end
+# end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,7 @@
 require 'docking_station'
 
 describe DockingStation do
-  it { is_expected.to respond_to (:release_bike) }
+  it "class docking station responds to release bike method" do
+  expect(subject).to respond_to(:release_bike)
+  end
 end
