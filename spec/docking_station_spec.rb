738b802f9f5b6871f1eb5573fa3bac39137b8da0
Julian George <jules104@gmail.com>
2019-08-23 15:27:59 +0100

    Add making stations release bikes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,5 +3,17 @@ require 'docking_station'
 describe DockingStation do
   it "class docking station responds to release bike method" do
   expect(subject).to respond_to(:release_bike)
-  end
+end
+
+it "releases new instance of Bike" do
+docking_station = DockingStation.new
+expect(docking_station.release_bike).to be_an_instance_of(Bike)
+end
+
+it "expects bike to be working" do
+docking_station = DockingStation.new
+bike = docking_station.release_bike
+expect(bike.working?).to eq(true)
+end
+
 end
